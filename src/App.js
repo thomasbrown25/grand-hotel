19abb0c97e83e42a28d9e1e5b07260039cbdb096
Thomas Brown <thobrown@microsoft.com>
2022-09-01 00:18:38 -0400

    added some responsiveness

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import AboutUs from './routes/about-us/about-us.component';
 import Home from './routes/home/home.component';
 
+import './media-queries.css';
+
 const App = () => {
     return (
         <Routes>
